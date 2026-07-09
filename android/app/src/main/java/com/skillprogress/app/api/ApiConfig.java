2ca68ca353e1e083af9924c957a90403871c72a8
fathmuiz <187072380+fathmuiz@users.noreply.github.com>
2026-07-09 11:55:08 +0700

    membersihkan cache filehtml

--- android/app/src/main/java/com/skillprogress/app/api/ApiConfig.java
+++ android/app/src/main/java/com/skillprogress/app/api/ApiConfig.java
@@ -1,9 +1,9 @@
 package com.skillprogress.app.api;
 
 public class ApiConfig {
-    // Ganti 192.168.xx.x dengan alamat IP server PHP jika menggunakan device hp.
+    // Ganti 192.168.18.87 dengan alamat IP server PHP jika menggunakan device hp.
     // 10.0.2.2 adalah alamat localhost mesin host saat dijalankan di emulator Android Studio.
-    public static final String BASE_URL = "http://aeby.whf.bz/backend/";
+    public static final String BASE_URL = "http://10.0.2.2/skillprogress/backend/";
 
     public static final String URL_REGISTER = BASE_URL + "auth/register.php";
     public static final String URL_LOGIN = BASE_URL + "auth/login.php";
